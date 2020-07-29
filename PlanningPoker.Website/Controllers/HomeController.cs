2fc99f4abe7afcb6f484720a5674fd093547a92e
harbargerdev <aharbarger@gmail.com>
2020-07-29 18:59:27 -0400

    Saving gamemaster and game

--- PlanningPoker.Website/Controllers/HomeController.cs
+++ PlanningPoker.Website/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PlanningPoker.Core.Entities;
 using PlanningPoker.Core.Utilities;
+using PlanningPoker.Website.Data;
 using PlanningPoker.Website.Models;
 
 namespace PlanningPoker.Website.Controllers
@@ -15,11 +16,13 @@ namespace PlanningPoker.Website.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IGameUtility _gameUtility;
+        private readonly GameContext _gameContext;
 
-        public HomeController(ILogger<HomeController> logger, IGameUtility gameUtility)
+        public HomeController(ILogger<HomeController> logger, IGameUtility gameUtility, GameContext gameContext)
         {
             _logger = logger;
             _gameUtility = gameUtility;
+            _gameContext = gameContext;
         }
 
         public IActionResult Index()
@@ -31,6 +34,11 @@ namespace PlanningPoker.Website.Controllers
         {
             Player gameMaster = new Player { PlayerId = Guid.NewGuid(), PlayerName = playerName, PlayerType = PlayerType.GameMaster };
             Game newGame = _gameUtility.InitializeGameFromGameMaster(gameMaster, gameName);
+
+            _gameContext.Add(gameMaster);
+            _gameContext.Add(newGame);
+            _gameContext.SaveChanges();
+            
             ViewBag.Game = newGame;
             return View();
         }
