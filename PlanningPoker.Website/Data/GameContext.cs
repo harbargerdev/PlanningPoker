4eb65dced3781bc77a0e69966c7897f0dadc1d50
harbargerdev <aharbarger@gmail.com>
2020-07-29 18:19:47 -0400

    Entity Framework

---
+++ PlanningPoker.Website/Data/GameContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlanningPoker.Core.Entities;
+
+namespace PlanningPoker.Website.Data
+{
+    public class GameContext : DbContext
+    {
+        public GameContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        // Entities
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+}
