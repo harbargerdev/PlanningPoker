45fec323996b00236b0d7775a725e416674bdc03
Andrew Harbarger <aharbarger@gmail.com>
2020-07-30 11:00:19 -0400

    Some refactoring and adding of new methods

--- PlanningPoker.Website/Controllers/HomeController.cs
+++ PlanningPoker.Website/Controllers/HomeController.cs
@@ -69,28 +69,71 @@ namespace PlanningPoker.Website.Controllers
         public IActionResult GameMasterZone([FromQuery] Guid gameId, [FromQuery] Guid cardId, [FromQuery] string cardNumber,
             [FromQuery] string cardSource, [FromQuery] string action)
         {
+            var game = _gameContext.Games.FirstOrDefault(g => g.GameId == gameId);
             Card card;
-            if (cardId == Guid.Empty)
+            if (cardId == Guid.Empty && game != null)
             {
                 card = _gameUtility.IntializeCardForGame(string.Empty, string.Empty);
+                game.ActiveCard = card;
                 _gameContext.Cards.Add(card);
+                _gameContext.Update(game);
             }   
             else
             {
                 card = _gameContext.Cards.FirstOrDefault(c => c.CardId == cardId);
                 card.CardNumber = cardNumber != null ? cardNumber : string.Empty;
                 card.CardSource = cardSource != null ? cardSource : string.Empty;
-                card.IsLocked = !action.Equals("Submit");
                 _gameContext.Update(card);
             }
             _gameContext.SaveChanges();
-
-            var game = _gameContext.Games.FirstOrDefault(g => g.GameId == gameId);
 
             ViewBag.Game = game;
             ViewBag.Card = card;
 
             return View();
+        }
+
+        public IActionResult GameMasterFinalizeVoting([FromQuery] Guid gameId, [FromQuery] Guid cardId)
+        {
+            var game = _gameContext.Games.FirstOrDefault(g => g.GameId == gameId);
+            var card = _gameContext.Cards.FirstOrDefault(c => c.CardId == cardId);
+
+            if (card != null && game != null)
+            {
+                card.IsLocked = true;
+                game.ActiveCard = null;
+
+                _gameContext.Update(card);
+                _gameContext.Update(game);
+
+                _gameContext.SaveChanges();
+            }
+
+            ViewBag.Game = game;
+            ViewBag.Card = card;
+
+            return View("GameMasterZone");
+        }
+
+        public IActionResult GameMasterNewCard([FromQuery] Guid gameId)
+        {
+            var game = _gameContext.Games.FirstOrDefault(g => g.GameId == gameId);
+            Card card = new Card();
+
+            if (game != null)
+            {
+                card = _gameUtility.IntializeCardForGame(string.Empty, string.Empty);
+                game.ActiveCard = card;
+
+                _gameContext.Cards.Add(card);
+                _gameContext.Update(game);
+                _gameContext.SaveChanges();
+            }
+
+            ViewBag.Game = game;
+            ViewBag.Card = card;
+
+            return View("GameMasterZone");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
