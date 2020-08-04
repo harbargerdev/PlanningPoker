b66d7e3315678553a3ce1b3796cda9c48f7458b0
harbargerdev <aharbarger@gmail.com>
2020-08-04 19:10:58 -0400

    voting.js for game master zone to set event listener and start connection

--- PlanningPoker.Website/wwwroot/js/voting.js
+++ PlanningPoker.Website/wwwroot/js/voting.js
@@ -6,8 +6,12 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/votehub").build();
 document.getElementById("finishButton").disabled = true;
 
 connection.on("VotingStatus", function (gameId, status) {
-    var currentGame = document.getElementById("gameIdTextBox").nodeValue;
+    var currentGame = document.getElementById("gameIdTextBox").value;
     if (gameId === currentGame && status === "complete") {
         document.getElementById("finishButton").disabled = false;
     }
+});
+
+connection.start().then(function () {
+    console.log('Connection started, awaiting messages ...');
 });
