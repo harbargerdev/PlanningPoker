90a6716e96258401d8322462ceac65faddf6a435
harbargerdev <aharbarger@gmail.com>
2020-08-07 17:58:03 -0400

    Lambda Function to clean up records older than a day

---
+++ PlanningPoker.Cleaner/PlanningPoker.Cleaner/Data/GameContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlanningPoker.Core.Entities;
+
+namespace PlanningPoker.Cleaner.Data
+{
+    public class GameContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=127.0.0.1;port=3306;Database=planningpoker;uid=planningpoker;pwd=Moxqzqyd47;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        // Entities
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+}
---
+++ PlanningPoker.Cleaner/PlanningPoker.Cleaner/Function.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Microsoft.EntityFrameworkCore;
+using PlanningPoker.Cleaner.Data;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace PlanningPoker.Cleaner
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            // Initialize count
+            int count = 0;
+
+            var _context = new GameContext();
+            var yesterday = DateTime.Now.AddDays(-1);
+            
+            // Get List of games older than yesterday
+            var games = _context.Games
+                .Include(g => g.Cards)
+                .Include(g => g.GameMaster)
+                .Include(g => g.Players)
+                .ToList()
+                .Where(g => g.GameTime <= yesterday);
+
+            foreach(var game in games)
+            {
+                if(game.ActiveCard != null)
+                {
+                    game.ActiveCard = null;
+                    _context.Update(game);
+                    _context.SaveChanges();
+                }
+
+                _context.Cards.RemoveRange(game.Cards);
+                _context.Players.Remove(game.GameMaster);
+                _context.Players.RemoveRange(game.Players);
+                _context.Games.Remove(game);
+
+                _context.SaveChanges();
+                count++;
+            }
+
+            // Return count of deleted objects
+            return count.ToString();
+        }
+    }
+}
